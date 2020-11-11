162ffec1b00d7815289f20ce842f2ad79c3cf830
40478631 <40478631@live.napier.ac.uk>
2020-11-11 10:23:15 +0630

    update travis by after adding sql database

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -28,7 +28,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(300);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db/test_db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(100);
