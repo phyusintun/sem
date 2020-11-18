a1c77f33eed106527c0dbb93d0e2d31a42cce618
40478631 <40478631@live.napier.ac.uk>
2020-11-18 22:17:48 +0630

    tested added and updated travis to debug 3

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060");
+    }
+
+    @Test
+    void testGetEmployee()
+    {
+        Employee emp = app.getEmployee(255530);
+        assertEquals(emp.emp_no, 255530);
+        assertEquals(emp.first_name, "Ronghao");
+        assertEquals(emp.last_name, "Garigliano");
+    }
+}
